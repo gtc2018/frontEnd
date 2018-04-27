958e5f5383bfe927faabe3b847aa78ef846edf88
gtc2018 <felix_alegria@gestecon.net>
2018-04-27 10:32:33 -0500

    Empleado Externo Modelo - Kellys Santa

---
+++ frontEnd/src/app/model/externalEmployee.ts
@@ -0,0 +1,33 @@
+import { ParentEntity } from './parentEntity.model';
+
+export class ExternalEmployeeModel extends ParentEntity{
+
+  public clienteId: string;
+
+  public cargoId: string;
+
+  public tipoEmpleado: string;
+
+  public tipoDocumento: string;
+
+  public numeroDocumento: string;
+
+  public nombres: string;
+
+  public apellidos: string;
+
+  public email: string;
+
+  public sexo: string;
+
+  public estado: number;
+
+  public direccion: string;
+
+  public telefono: string;
+
+  public extension: string;
+
+  public usuarioCreacion: string;
+
+}
